20f6fe983c2641942bd991640836548f040e39d5
SNRin <58441867+SNRin@users.noreply.github.com>
2020-09-03 14:41:31 -0500

    fonts

--- p5/Unit1/fonts/sketch.js
+++ p5/Unit1/fonts/sketch.js
@@ -1,18 +1,22 @@
-var f;
+var f1, f2;
 
 function setup() {
   createCanvas(800,800);
 
-f = loadFont ("assets/stocky.ttf");
+f1 = loadFont ("assets/fast99.ttf");
+f2 = loadFont ("assets/leadcoat.ttf");
 
 textAlign (CENTER);
 }
 
 function draw() {
   background(100);
-  textFont(f);
+  textFont(f1);
   textSize(60);
-//add another font
-  text("favorite quote/lyric", width/2, height/2);
+
+  text("font number one", width/2, height/2 - 100);
+  textFont(f2);
+  textSize(30);
+  text("font number two", width/2, height/2 + 100);
 
 }
